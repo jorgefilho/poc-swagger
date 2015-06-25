50ebb68fd71432eb125d618b3852f85ed5affcef
jorgefilho <papachristo@gmail.com>
2015-06-24 22:31:31 -0300

    Starting the include of swagger's settings.

--- poc-swagger/src/main/java/io/github/jorgefilho/swagger/resource/UserResource.java
+++ poc-swagger/src/main/java/io/github/jorgefilho/swagger/resource/UserResource.java
@@ -2,6 +2,8 @@ package io.github.jorgefilho.swagger.resource;
 
 import io.github.jorgefilho.swagger.domain.UserRequest;
 import io.github.jorgefilho.swagger.resource.constants.ResourcePath;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import java.io.Serializable;
 
@@ -14,12 +16,14 @@ import javax.ws.rs.core.Response;
 
 @Path(ResourcePath.USER_PATH)
 @Produces ({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON})
+@Api(value=ResourcePath.USER_PATH, description = "Operations about users")
 public class UserResource implements Serializable {
 
 	private static final long serialVersionUID = -8533969043792887825L;
 
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON})	
+	@ApiOperation(value = "Create user", notes = "Creates a single user", position = 1)
 	public Response insert(UserRequest user){
 		// TODO: 
 		return Response.ok().build();
